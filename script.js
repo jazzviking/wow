6047fad133201116bc11319984229c6e10601296
Jon Tucker <jazzviking@gmail.com>
2022-05-03 23:24:55 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const main = document.querySelector('.container');
+const toggleBtn = document.querySelector('.toggle-loading');
+
+const ellipses = `<div class="lds-ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>`;
+
+toggleBtn.addEventListener('click', () => {
+  showFortune();
+});
+
+async function showFortune() {
+  main.innerHTML = ellipses;
+
+  let response = await fetch('https://zenquotes.io/api/random/');
+  let fortune = await response.json();
+
+  main.innerHTML = `<blockquote>"${fortune[0].q}"<blockquote><em class="author">&mdash; ${fortune[0].a}</em>`;
+}
+
+showFortune();
